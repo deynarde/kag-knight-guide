75dc6fd463dfa3eae75968d3f860148abac82cab
deynarde <deynarde@gmail.com>
2019-01-31 20:37:44 +0100

    chapter 1 webm

--- _deploy.sh
+++ _deploy.sh
@@ -11,7 +11,6 @@ git config --global user.name "deynarde"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
-cp -r ../imgs/* ./imgs
 git add --all *
 git commit -m "Update the book" || true
 git push -q origin gh-pages
